fdf8069a7733e2b2db0215fab10ab793c80098d8
ipuppet <zimingw.ol@outlook.com>
2023-02-11 01:36:37 +0800

    update

--- README.md
+++ README.md
@@ -3,8 +3,7 @@
 ### Install
 
 JSBox:
-- b23clean.js: jsbox://import?url=https%3A%2F%2Fraw.githubusercontent.com%2Fipuppet%2Fb23clean%2Fmaster%2Fdist%2Fb23clean.js
+- b23clean.js: [jsbox://import?url=https%3A%2F%2Fraw.githubusercontent.com%2Fipuppet%2Fb23clean%2Fmaster%2Fdist%2Fb23clean.js](jsbox://import?url=https%3A%2F%2Fraw.githubusercontent.com%2Fipuppet%2Fb23clean%2Fmaster%2Fdist%2Fb23clean.js)
 
 Taio:
-- b23clean-en: taio://actions?action=import&url=https%3A%2F%2Fraw.githubusercontent.com%2Fipuppet%2Fb23clean%2Fmaster%2Fdist%2Fb23clean-en.json
-- b23clean-zh-Hans: taio://actions?action=import&url=https%3A%2F%2Fraw.githubusercontent.com%2Fipuppet%2Fb23clean%2Fmaster%2Fdist%2Fb23clean-zh-Hans.json
+- b23clean: [taio://actions?action=import&url=https%3A%2F%2Fraw.githubusercontent.com%2Fipuppet%2Fb23clean%2Fmaster%2Fdist%2Fb23clean.json](taio://actions?action=import&url=https%3A%2F%2Fraw.githubusercontent.com%2Fipuppet%2Fb23clean%2Fmaster%2Fdist%2Fb23clean.json)
--- scripts/app.js
+++ scripts/app.js
@@ -1,4 +1,4 @@
-const { Toast } = require("./libs/easy-jsbox")
+const { Toast } = require("./libs/toast")
 
 function getUrls() {
     const input = (() => {
@@ -15,35 +15,66 @@ function getUrls() {
     return input.match(regex) ?? []
 }
 
+async function cleanUrl(b23url) {
+    if (b23url.indexOf("bilibili.com") === -1 && b23url.indexOf("b23.tv") === -1) {
+        throw new Error($l10n("b23clean.noBiliUrl"))
+    }
+
+    let url = b23url
+    if (b23url.indexOf("b23.tv") >= 0) {
+        const resp = await $http.get(b23url)
+        url = resp.response.url
+    }
+
+    const queryStart = url.indexOf("?")
+    if (queryStart > -1) {
+        url = url.substring(0, queryStart - 1)
+    }
+
+    return url
+}
+
 async function main() {
-    const b23url = getUrls()[0]
-    if (!b23url) {
-        Toast.warning($l10n("b23clear.noUrl"))
-    } else if (b23url.indexOf("bilibili.com") === -1 && b23url.indexOf("b23.tv") === -1) {
-        Toast.warning($l10n("b23clear.noBiliUrl"))
-    } else {
-        Toast.info($l10n("b23clear.converting"), { font: $font(20) })
-        let url
-        if (b23url.indexOf("b23.tv")) {
-            const resp = await $http.get(b23url)
-            url = resp.response.url
+    const loading = Toast.info($l10n("b23clean.converting"), { displayTime: 100 })
+
+    try {
+        const b23url = getUrls()
+        if (b23url.length === 0) {
+            throw new Error($l10n("b23clean.noUrl"))
         }
 
-        const bvUrl = url.substring(0, url.indexOf("?") - 1)
-        $ui.alert({
-            title: $l10n("b23clear.success"),
-            message: bvUrl,
+        let url = b23url[0]
+        if (b23url.length > 1) {
+            const urlSelected = await $ui.menu({
+                items: b23url
+            })
+            url = b23url[urlSelected.index]
+        }
+
+        if (url.indexOf("bilibili.com") === -1 && url.indexOf("b23.tv") === -1) {
+            throw new Error($l10n("b23clean.noBiliUrl"))
+        }
+
+        url = await cleanUrl(url)
+        loading.remove()
+
+        await $ui.alert({
+            title: $l10n("b23clean.success"),
+            message: url,
             actions: [
                 { title: $l10n("OK") },
                 {
                     title: $l10n("COPY"),
                     handler: () => {
-                        $clipboard.text = bvUrl
+                        $clipboard.text = url
                         Toast.success($l10n("COPIED"))
                     }
                 }
             ]
         })
+    } catch (error) {
+        loading.remove()
+        $delay(0.5, () => Toast.error(String(error)))
     }
 }
 
